23cb28279e31fa19df5df9c6c0ebf6b936cd30ec
angelsophia1 <angelsunyulei@126.com>
2019-07-25 15:44:58 +0800

    Initial commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject effectPrefab;
+    public GameObject decalPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray,out hitInfo,100f))
+        {
+            //Instantiate(effectPrefab,hitInfo.point,Quaternion.LookRotation(hitInfo.normal));
+
+            Instantiate(decalPrefab,hitInfo.point,Quaternion.FromToRotation(Vector3.up,hitInfo.normal));
+        }
+    }
+}
